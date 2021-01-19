bc9003a689ed4bc81f9f296a3ae73b1d2fa47989
Shuvo Shuvo <shuvonaim123@gmail.com>
2021-01-19 23:01:30 +0600

    facbook login api added

---
+++ src/components/FacbookLogin.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import FacebookLogin from 'react-facebook-login';
+
+class FacbookLogin extends Component {
+    state = {
+        isLoggedIn: false,
+        userId: "",
+        name: "",
+        email: "",
+        picture: ""
+    }
+
+    responseFacebook = (response) => {
+        console.log(response)
+        this.setState({
+            isLoggedIn: true,
+            userId: response.userId,
+            name: response.name,
+            email: response.email,
+            picture: response.picture.data.url
+        });
+    }
+
+    componentClicked = () => console.log("clicked");
+
+    render() {
+        let fbContent;
+        if (this.state.isLoggedIn) {
+            fbContent = (
+              <div
+                style={{
+                  width: "400px",
+                  margin: "auto",
+                  background: "#3b3a38",
+                  padding: "20px"
+                }}
+              >
+                <img src={this.state.picture} alt={this.state.name} />
+                <h2 >Welcome {this.state.name}</h2>
+                Email: {this.state.email}
+              </div>
+            );
+          } else {
+
+            fbContent = (
+
+                <FacebookLogin
+                    appId="407383763654068"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    onClick={this.componentClicked}
+                    callback={this.responseFacebook} />
+
+            );
+
+        }
+        return (
+            <div>
+                {fbContent}
+                
+            </div>
+        );
+    }
+}
+
+export default FacbookLogin;
